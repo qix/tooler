2fe858924bbfbd35f3df2d63774b1d87cc190640
Josh Yudaken <j@yud.co.za>
2024-02-10 16:31:16 -0600

    0.1.5-rc1 with version link

--- tooler/version.py
+++ tooler/version.py
@@ -1,5 +1,4 @@
-
-__author__ = 'Josh Yudaken'
-__author_email__ = 'j@yud.co.za'
-__url__ = 'http://qix.github.io/tooler'
-__version__ = '0.1.4'
+__author__ = "Josh Yudaken"
+__author_email__ = "j@yud.co.za"
+__url__ = "http://qix.github.io/tooler"
+__version__ = "0.1.5-rc1"
